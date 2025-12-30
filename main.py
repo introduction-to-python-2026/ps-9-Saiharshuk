6aed181a8eb489256456cc762c077650a48e599a
Saiharshuk <sai.harshuk@live.biu.ac.il>
2025-12-30 16:34:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,26 @@
 import pandas as pd
-import joblib
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
-
-df = pd.read_csv("parkinsons.csv")
+df = pd.read_csv('parkinsons.csv')
 df = df.dropna()
-
-X = df[["MDVP:Fo(Hz)", "MDVP:Jitter(%)"]]
-y = df["status"]
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+df.head()
+x = df[['MDVP:Fo(Hz)', 'MDVP:Jitter(%)']]
+y = df['status']
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
 
 scaler = MinMaxScaler(feature_range=(0, 1))
-X_train_scaled = scaler.fit_transform(X_train)
-X_test_scaled = scaler.transform(X_test)
+x_scaled = scaler.fit_transform(x)
+from sklearn.model_selection import train_test_split
 
-model = KNeighborsClassifier(n_neighbors=5)
-model.fit(X_train_scaled, y_train)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+from sklearn.neighbors import KNeighborsClassifier
 
-y_pred = model.predict(X_test_scaled)
+model = KNeighborsClassifier()
+model.fit(x_scaled, y)
+from sklearn.metrics import accuracy_score
+
+y_pred = model.predict(x_test)
 accuracy = accuracy_score(y_test, y_pred)
 print(accuracy)
+import joblib
 
-joblib.dump(model, "my_model.joblib")
-joblib.dump(scaler, "scaler.joblib")
+joblib.dump(model, 'my_model.joblib')
